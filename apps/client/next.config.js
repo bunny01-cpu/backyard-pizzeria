9ceae8b22450528e51e201401c556a6d4b8cde3b
bunny01-cpu <bunny01-cpu@users.noreply.github.com>
2026-05-26 22:20:27 +0530

    fix: remove Windows-only NODE_OPTIONS fix next config for Vercel

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        forceSwcTransforms: false,
+    },
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+        return config;
+    },
+};
 
 module.exports = nextConfig;
